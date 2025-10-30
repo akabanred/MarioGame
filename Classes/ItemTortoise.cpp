ada219bf49344e2a29a61410ab617aad41dd6157
MQ-1990 <quang.tranminh@hcmut.edu.vn>
2025-10-31 01:20:34 +0700

    Update ItemTortoise.cpp

--- Classes/ItemTortoise.cpp
+++ Classes/ItemTortoise.cpp
@@ -1,29 +1,29 @@
 #include "ItemTortoise.h"
 #include "Mario.h"
-
-ItemTortoise* ItemTortoise::create(ValueMap& map)
+#include "AudioEngine.h"
+ItemTortoise *ItemTortoise::create(ValueMap &map)
 {
-	ItemTortoise * pRet = new ItemTortoise();
-	if (pRet&&pRet->init(map)){
+	ItemTortoise *pRet = new ItemTortoise();
+	if (pRet && pRet->init(map))
+	{
 		pRet->autorelease();
 	}
-	else{
+	else
+	{
 		delete pRet;
 		pRet = nullptr;
 	}
 
 	return pRet;
-
 }
 
-
-bool ItemTortoise::init(ValueMap& map)
+bool ItemTortoise::init(ValueMap &map)
 {
 	ItemCanMove::init();
 	_type = Item::IT_MushroomMonster;
 	setPositionByProperty(map);
-	//setDisplayFrameWithAnimationName("mushroomMoving", 0);
-	
+	// setDisplayFrameWithAnimationName("mushroomMoving", 0);
+
 	m_initXV = -40;
 	_speedY = 0;
 	_speedX = m_initXV;
@@ -34,39 +34,40 @@ bool ItemTortoise::init(ValueMap& map)
 	this->runAction(RepeatForever::create(Animate::create(AnimationCache::getInstance()->getAnimation(ANI_tortoiseLeftMoving))));
 	return true;
 }
-void ItemTortoise::moveCheck(float dt){
+void ItemTortoise::moveCheck(float dt)
+{
 	if (m_state == SLEEP)
 		return;
-	
-	if (canMoveHorizontal(dt)){
+
+	if (canMoveHorizontal(dt))
+	{
 		moveHorizontal(dt);
 	}
-	else{
-		
+	else
+	{
+
 		_speedX = -_speedX;
-		
+
 		updateStatus();
 	}
 
-	if (canMoveDown(dt)){
+	if (canMoveDown(dt))
+	{
 		moveDown(dt);
 	}
-	else{
+	else
+	{
 		_speedY = 0;
 	}
 
-	if (isOutOfWindow()){
+	if (isOutOfWindow())
+	{
 		removeFromParent();
 	}
-
 }
 
-
-
-
-
-
-void ItemTortoise::collisionCheck(float dt){
+void ItemTortoise::collisionCheck(float dt)
+{
 	if (m_bIsDead)
 		return;
 	if (_isGodMode)
@@ -74,97 +75,98 @@ void ItemTortoise::collisionCheck(float dt){
 	Rect rcMario = Mario::getInstance()->getBoundingBox();
 	Rect rcItem = this->getBoundingBox();
 
-	if (rcMario.intersectsRect(rcItem)){
+	if (rcMario.intersectsRect(rcItem))
+	{
 
-		if (m_state==NORMAL){
-			if (Mario::getInstance()->getSpeedY() <= 0 && rcMario.getMinY() > rcItem.getMaxY() - rcItem.size.height / 2){
+		if (m_state == NORMAL)
+		{
+			if (Mario::getInstance()->getSpeedY() <= 0 && rcMario.getMinY() > rcItem.getMaxY() - rcItem.size.height / 2)
+			{
+				AudioEngine::play2d(SOUND_enemy_down);
 				_speedX = 0;
 				m_state = SLEEP;
-				
-				//ÎÞµÐÒ»¶ÎÊ±¼ä
+
+				// ï¿½Þµï¿½Ò»ï¿½ï¿½Ê±ï¿½ï¿½
 				_isGodMode = true;
-				
-				//ÈÃÂíÀï°Âµ¯³öÈ¥
+
+				// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Âµï¿½ï¿½ï¿½È¥
 				Mario::getInstance()->jump(100);
-				
+
 				scheduleOnce(CC_SCHEDULE_SELECTOR(ItemTortoise::cancelGodModeCallback), 0.2f);
 
-				//¹ýÒ»¶ÎÊ±¼ä¸´»î
+				// ï¿½ï¿½Ò»ï¿½ï¿½Ê±ï¿½ä¸´ï¿½ï¿½
 				scheduleOnce(CC_SCHEDULE_SELECTOR(ItemTortoise::reLiveCallback), 5.0f);
-
 			}
-			else{
-				//ÂíÀï°ÂËÀÍö
+			else
+			{
+				// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 				Mario::getInstance()->die(false);
-			
 			}
 			return;
-			
 		}
 
-		if (m_state==SLEEP){
-			_speedX = Mario::getInstance()->getPositionX() < getPositionX() ?
-				-m_initXV * 3 : m_initXV * 3;
+		if (m_state == SLEEP)
+		{
+			_speedX = Mario::getInstance()->getPositionX() < getPositionX() ? -m_initXV * 3 : m_initXV * 3;
 			m_state = CRAZY;
-			
+
 			return;
 		}
-		if (m_state == CRAZY){
-			if (Mario::getInstance()->getSpeedY() <= 0 && rcMario.getMinY() > rcItem.getMaxY() - rcItem.size.height / 2){
+		if (m_state == CRAZY)
+		{
+			if (Mario::getInstance()->getSpeedY() <= 0 && rcMario.getMinY() > rcItem.getMaxY() - rcItem.size.height / 2)
+			{
+				AudioEngine::play2d(SOUND_enemy_down);
 				this->stopAllActions();
 				this->runAction(RepeatForever::create(Animate::create(AnimationCache::getInstance()->getAnimation("tortoiseDead"))));
 
 				_speedX = 0;
 				m_state = SLEEP;
-				//ÎÞµÐÒ»¶ÎÊ±¼ä
+				// ï¿½Þµï¿½Ò»ï¿½ï¿½Ê±ï¿½ï¿½
 				_isGodMode = true;
 				scheduleOnce(CC_SCHEDULE_SELECTOR(ItemTortoise::cancelGodModeCallback), 0.2f);
 
-				//¹ýÒ»¶ÎÊ±¼ä¸´»î
+				// ï¿½ï¿½Ò»ï¿½ï¿½Ê±ï¿½ä¸´ï¿½ï¿½
 				scheduleOnce(CC_SCHEDULE_SELECTOR(ItemTortoise::reLiveCallback), 0.2f);
-
 			}
-			else{
+			else
+			{
 				if (Mario::getInstance()->isGodMode())
 					return;
 				Mario::getInstance()->die();
-
 			}
-			
 
 			return;
 		}
-		
 	}
-
 }
 
-void ItemTortoise::cancelGodModeCallback(float dt){
+void ItemTortoise::cancelGodModeCallback(float dt)
+{
 	_isGodMode = false;
 }
-void ItemTortoise::reLiveCallback(float dt){
-	if (m_state == SLEEP){
+void ItemTortoise::reLiveCallback(float dt)
+{
+	if (m_state == SLEEP)
+	{
 
 		m_state = NORMAL;
 		_speedX = m_initXV;
 		stopAllActions();
 		this->runAction(RepeatForever::create(Animate::create(
 			AnimationCache::getInstance()
-			->getAnimation(_speedX > 0 ?
-			"tortoiseRightMoving" : "tortoiseLeftMoving"))));
-
+				->getAnimation(_speedX > 0 ? "tortoiseRightMoving" : "tortoiseLeftMoving"))));
 	}
 }
-void ItemTortoise::updateStatus(){
+void ItemTortoise::updateStatus()
+{
 
-	if (m_state == NORMAL){
+	if (m_state == NORMAL)
+	{
 		stopAllActions();
 
-		
-	this->runAction(RepeatForever::create(Animate::create(
-				AnimationCache::getInstance()
-				->getAnimation(_speedX > 0 ?
-				"tortoiseRightMoving" : "tortoiseLeftMoving"))));
+		this->runAction(RepeatForever::create(Animate::create(
+			AnimationCache::getInstance()
+				->getAnimation(_speedX > 0 ? "tortoiseRightMoving" : "tortoiseLeftMoving"))));
 	}
-
 }
